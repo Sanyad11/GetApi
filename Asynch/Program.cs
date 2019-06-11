1c7693e96d5a9441a37c1977fe839c81e2360c30
k670 <2ki15b.melnik@email.com>
2019-06-11 15:23:51 +0300

    first commit

---
+++ Asynch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace GetApi
+{
+    class Program
+{
+        static async void GetAsync()
+        {
+            Task[] tasks = new Task[2]
+               {
+                    new Task(() => Get("http://get-simple.info/api/")),
+                    new Task(() => Get("http://get-simple.info/wiki/ru"))
+               };
+            foreach (var t in tasks)
+                t.Start();
+            Task.WaitAll(tasks);
+        }
+        static async void Get(string str)
+        {
+            var client = new HttpClient();
+            var response =  client.GetAsync(str).Result;
+            var responseContent = response.Content;
+            string responseS = responseContent.ReadAsStringAsync().Result;
+           //Console.WriteLine(responseS);
+        }
+
+        static void Main()
+        {
+            Stopwatch sw;
+            sw = Stopwatch.StartNew();
+            Get("http://get-simple.info/wiki/ru");
+            Get("http://get-simple.info/api/");
+            Console.WriteLine("time Sync = " + sw.ElapsedMilliseconds);
+            sw.Reset();
+            sw = Stopwatch.StartNew();
+            GetAsync();
+            Console.WriteLine("time Async = " + sw.ElapsedMilliseconds);
+            sw.Stop();
+
+
+            Console.ReadLine();
+        }
+
+}
+}
+
